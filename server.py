9037c8c0f062fc6e8e43f3eec35fdbb1837eb358
Kevin Chen <kevinrchen0@gmail.com>
2019-11-18 19:31:43 -0500

    Application code

--- server.py
+++ server.py
@@ -1,1 +1,39 @@
+from flask import Flask, request
+from flask import render_template
+import tensorflow.keras as keras
+import sys, json
+import numpy as np
+application = Flask(__name__)
 
+stored_model = None
+
+
+@application.route("/")
+def hello():
+    return render_template('index.html')
+
+@application.route("/predict", methods=['POST'])
+def classifyCharacter():
+    global stored_model
+
+    body = request.get_json()
+
+    reshapedData = np.array(body['data'])
+    reshapedData = reshapedData.reshape(1,28,28,1)
+
+    return json.dumps( { 'prediction' : int(stored_model.predict_classes( reshapedData )[0]) } )
+
+@application.route("/")
+def hello():
+    return render_template('index.html')
+
+@application.route("/predict", methods=['POST'])
+def classifyCharacter():
+    global stored_model
+
+    body = request.get_json()
+
+    reshapedData = np.array(body['data'])
+    reshapedData = reshapedData.reshape(1,28,28,1)
+
+    return json.dumps( { 'prediction' : int(stored_model.predict_classes( reshapedData )[0]) } )
